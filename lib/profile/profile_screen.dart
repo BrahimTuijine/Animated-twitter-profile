4d43f8df88498324505ca74b094a212ad42b19d6
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2025-09-24 00:21:03 +0100

    feat: add user information display to WhatsappProfilePage

--- lib/profile/profile_screen.dart
+++ lib/profile/profile_screen.dart
@@ -13,6 +13,31 @@ class WhatsappProfilePage extends StatelessWidget {
         body: CustomScrollView(
           slivers: [
             SliverPersistentHeader(delegate: WhatsappAppBar(), pinned: true),
+            SliverToBoxAdapter(
+              child: Container(
+                margin: const EdgeInsets.only(top: 60),
+                child: Column(
+                  children: [
+                    Text(
+                      '50 611 513',
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      'Brahim tuijine',
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const WhatsappProfileBody(),
           ],
         ),
       ),
@@ -30,6 +55,16 @@ class WhatsappAppBar extends SliverPersistentHeaderDelegate {
     begin: Colors.grey[800],
     end: Colors.white,
   );
+
+  final phoneNumberFontSizeTween = Tween<double>(begin: 20, end: 15);
+
+  final phoneNumberTranslationTween = Tween<double>(begin: 80, end: 15);
+
+  final profileLeftPositionTween = Tween<double>(begin: 160, end: 40);
+
+  final profileTopPositionTween = Tween<double>(begin: 80, end: 4);
+
+  final profileSizeTween = Tween<double>(begin: 3, end: 1);
 
   @override
   Widget build(
@@ -62,38 +97,38 @@ class WhatsappAppBar extends SliverPersistentHeaderDelegate {
                 color: appBackIconsColorTween.transform(relativeScroll),
               ),
             ),
-            Positioned(top: 15, left: 90, child: Text('+33 333333333333')),
+            if (shrinkOffset > 16)
+              Positioned(
+                top: phoneNumberTranslationTween.transform(relativeScroll),
+                left: 90,
+                child: Text(
+                  '+216 50 611 513',
+                  style: TextStyle(
+                    fontSize: phoneNumberFontSizeTween.transform(
+                      relativeScroll,
+                    ),
+                  ),
+                ),
+              )
+            else
+              SizedBox.shrink(),
+            Positioned(
+              top: profileTopPositionTween.transform(relativeScroll),
+              left: profileLeftPositionTween.transform(relativeScroll),
+              child: Transform.scale(
+                scale: profileSizeTween.transform(relativeScroll),
+                child: const CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEgzwHNJhsADqquO7m7NFcXLbZdFZ2gM73x8I82vhyhg&s",
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
-
-  // Widget displayPhoneNumber(double relativeFullScrollOffset) {
-  //   if (relativeFullScrollOffset >= 0.8) {
-  //     return Transform(
-  //       transform: Matrix4.identity()
-  //         ..translate(
-  //           0.0,
-  //           phoneNumberTranslateTween.transform(
-  //             (relativeFullScrollOffset - 0.8) * 5,
-  //           ),
-  //         ),
-  //       child: Text(
-  //         "+3 3333333333",
-  //         style: TextStyle(
-  //           fontSize: phoneNumberFontSizeTween.transform(
-  //             (relativeFullScrollOffset - 0.8) * 5,
-  //           ),
-  //           color: Colors.white,
-  //           fontWeight: FontWeight.w500,
-  //         ),
-  //       ),
-  //     );
-  //   }
-
-  //   return const SizedBox.shrink();
-  // }
 
   @override
   double get maxExtent => 120;
@@ -104,4 +139,32 @@ class WhatsappAppBar extends SliverPersistentHeaderDelegate {
   @override
   bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
       true;
+}
+
+class WhatsappProfileBody extends StatelessWidget {
+  const WhatsappProfileBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildListDelegate(const [
+        SizedBox(height: 20),
+        ListTile(
+          title: Text("Custom Notifications"),
+          leading: Icon(Icons.notification_add),
+        ),
+        ListTile(
+          title: Text("Disappearing messages"),
+          leading: Icon(Icons.message),
+        ),
+        ListTile(
+          title: Text("Mute Notifications"),
+          leading: Icon(Icons.mic_off),
+        ),
+        ListTile(title: Text("Media visibility"), leading: Icon(Icons.save)),
+        // to fill up the rest of the space to enable scrolling
+        SizedBox(height: 550),
+      ]),
+    );
+  }
 }
